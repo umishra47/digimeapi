084e2ce9e85c7c9de478d7c36efed3568c190b17
enbake_ukumar <ukumar@enbake.com>
2014-10-18 13:50:05 +0530

    issue in reset_password api resolved

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ActionMailer::Base
 
-default_url_options[:host] = "localhost:3000"
+# default_url_options[:host] = "localhost:3000"
+default_url_options[:host] = "digimeapi.herokuapp.com"
 
 def password_instruction_email(user)
   @user = user
